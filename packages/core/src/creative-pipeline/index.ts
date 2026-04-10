2c0ffab501ae0c47c97aa1833dad2096d565cd08
jsonljc <jasonlijc@gmail.com>
2026-04-10 09:13:38 +0800

    feat(core): add creative-pipeline barrel exports and package export map

---
+++ packages/core/src/creative-pipeline/index.ts
@@ -0,0 +1,5 @@
+export { inngestClient } from "./inngest-client.js";
+export type { CreativePipelineEvents } from "./inngest-client.js";
+export { createCreativeJobRunner, executeCreativePipeline } from "./creative-job-runner.js";
+export { runStage, getNextStage } from "./stages/run-stage.js";
+export type { StageName, StageInput } from "./stages/run-stage.js";
